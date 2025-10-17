e9ae7183484fdd898130afa67ec57fe9bad096e8
triuduongg <triuduongg374@gmail.com>
2025-10-18 01:15:17 +0700

    Delete uploads directory

--- uploads/logfunctions/LoginLog.php
+++
@@ -1,9 +0,0 @@
-<?php
-function LoginLog($email, $text){
-    $date =date('d/m/y h:m:s');
-    $path = './uploads/logs/'.$email.'.txt';
-    $handle = fopen($path, 'a');
-    $content =$date. ' - '. $email.' '.$text . PHP_EOL;
-    fwrite($handle, $content);
-    fclose($handle);
-}
